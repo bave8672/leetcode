369bc629b3158c5316d492d6a494094cff1f19e7
Ben <avesbenjamin@gmail.com>
2021-01-20 13:03:24 +1100

    add invert-binary-tree

---
+++ src/invert-binary-tree/invert-binary-tree.ts
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/invert-binary-tree/
+ *
+ * <problem description>
+ */
+
+export function invertTree(root: TreeNode | null): TreeNode | null {
+    if (!root) {
+        return null;
+    }
+    const left = invertTree(root.left);
+    root.left = invertTree(root.right);
+    root.right = left;
+    return root;
+}
+
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
---
+++ src/number-of-ways-to-reconstruct-a-tree/number-of-ways-to-reconstruct-a-tree.spec.ts
@@ -0,0 +1,84 @@
+import { checkWays } from "./number-of-ways-to-reconstruct-a-tree";
+
+xdescribe("number-of-ways-to-reconstruct-a-tree", () => {
+    spec(
+        [
+            [1, 2],
+            [2, 3],
+        ],
+        1,
+    );
+    spec(
+        [
+            [1, 2],
+            [2, 3],
+            [1, 3],
+        ],
+        2,
+    );
+    spec(
+        [
+            [1, 2],
+            [2, 3],
+            [2, 4],
+            [1, 5],
+        ],
+        0,
+    );
+    spec(
+        [
+            [1, 2],
+            [1, 3],
+            [1, 4],
+            [1, 5],
+            [1, 6],
+            [1, 7],
+            [2, 3],
+            [2, 4],
+            [2, 6],
+            [2, 7],
+            [3, 4],
+            [3, 7],
+        ],
+        1,
+    );
+    spec(
+        [
+            [1, 2],
+            [1, 3],
+            [1, 4],
+            [1, 5],
+            [1, 6],
+            [1, 7],
+            [1, 8],
+            [2, 3],
+            [2, 4],
+            [2, 6],
+            [2, 7],
+            [2, 8],
+            [3, 4],
+            [3, 7],
+            [3, 8],
+            [4, 8],
+        ],
+        2,
+    );
+    spec(
+        [
+            [3, 4],
+            [2, 3],
+            [4, 5],
+            [2, 4],
+            [2, 5],
+            [1, 5],
+            [1, 4],
+        ],
+        0,
+    );
+
+    function spec(pairs: number[][], expected: number) {
+        it(`${JSON.stringify({ pairs, expected })}`, () => {
+            expect(checkWays(pairs)).toEqual(expected);
+        });
+    }
+});
